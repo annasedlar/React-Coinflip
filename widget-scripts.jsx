add245a7e8446e51c9b26ade0a9363146f811678
Anna Sedlar <annasedlar@gmail.com>
2017-01-05 09:30:57 -0500

    basic react markup and components

--- widget-scripts.jsx
+++ widget-scripts.jsx
@@ -1,10 +1,55 @@
+function ProductCategoryRow(props){
+	return(
+		<tr>
+			<td>Sporting Goods</td>
+		</tr>
+	)
+}
+
+function ProductRow(props){
+	return(
+		<tr>
+			<td>Football</td>
+			<td>$19.99</td>
+		</tr>
+	)
+}
+
+function ProductTable(props){
+	return(
+		<div>
+			<ProductCategoryRow />
+			<ProductRow />
+		</div>
+	)
+}
+
+function SearchBar(props){
+	return(
+		<input type="text" />)
+}
+
+// ^ same as class SearchBar extends React.Component{
+// 	render(){
+// 		return
+// 	}
+// } this way is much more powerful
+
+function FilterableProductTable(props){
+	return(
+		<div>
+			<SearchBar />
+			<ProductTable />
+		</div>)
+}
+
 // We aren't using a class *yet). So we have to include props as a parameter because there is no 'this'.
 
 function Application(props){
 	//must return a virtual DOM element
 	return(
-		<h1>Test</h1>
-		)
+		<FilterableProductTable />
+	)
 }
 //2 args, the what and the where
 ReactDOM.render(
